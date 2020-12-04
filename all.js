84e967d469f2f664c5524fad70f54c3b76f3cdba
7oy <52928666+7oy@users.noreply.github.com>
2020-12-05 01:03:42 +0600

    Update all.js

--- all.js
+++ all.js
@@ -57,7 +57,7 @@ click_encode(){
 	};
 },
 event_decode(){//при нажатии по кнопке декодирует сообщение
-	if((event.type=='keyup')&&(event.code='Enter')) return;
+	if((event.type=='keyup')&&(event.code!='Enter')) return;
 	let temp;
 	temp=event.ctrlKey?this.form.textarea.value:this.text;
 	if((this.form.text.value.length==0)||(this.text==null)) _fail.red('Необходимые данные для рассшифровки отсутсвуют!');
