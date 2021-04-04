aeaa8f1859637a86118e50574738f5aa73403bde
7oy <52928666+7oy@users.noreply.github.com>
2021-04-04 20:14:10 +0600

    Update all.js

--- all.js
+++ all.js
@@ -3,7 +3,8 @@
 let main={
 body:null,
 form:null,
-address:'https://script.google.com/macros/s/AKfycbwqcH-Hzjml-mqk_PMOG-FZleVFhk27oiVxhApMgSPM1Zq4zg/exec',
+address:'https://script.google.com/macros/s/AKfycbxkCdqx84NGqlYgyYGpGn8UJD_1N-iD-nBo2UqkZDAyzX08iNAYY-9RLDwrK4uHH2U/exec',
+
 text:null,
 show:null,
 link:null,
@@ -59,8 +60,7 @@ click_encode(){
 event_decode(){//при нажатии по кнопке декодирует сообщение
 	if((event.type=='keyup')&&(event.key!='Enter')) return;
 	let temp;
-	temp=(event.key=='Control')?this.form.textarea.value:this.text;
-	Console.log(temp);
+	temp=(event.ctrlKey)?this.form.textarea.value:this.text;
 	if((this.form.text.value.length==0)||(this.text==null)) _fail.red('Необходимые данные для рассшифровки отсутсвуют!');
 	else {
 		this.form.textarea.value=this.decrypt(this.form.text.value,temp);//this.text
@@ -198,7 +198,8 @@ async ajax(url,obj=0){//AJAX модуль
 		body:JSON.stringify(obj),
 		signal
 	}
-	const response=await fetch('https://wild-forest-b24f.xer.workers.dev/?'+url,options);
+	const response=await fetch('https://mighty-lowlands-35866.herokuapp.com/'+url,options);
+	//const response=await fetch(url,options);
 	if(!response.ok) throw new Error (response.status+' '+response.statusText);
 	const text=await response.text();
 	//if (text.toLowerCase().indexOf("error")!=-1) throw new Error(text); //проверяем проблемы
@@ -254,9 +255,9 @@ delete(){ //удаляет сообщение по клику
 
 let sha512={//(str) 
 	charsize:8,
-/**/int64:function(msint_32, lsint_32){//переделать на ES6
-		this.highOrder = msint_32;
-		this.lowOrder = lsint_32;
+	int64(msint_32, lsint_32){
+		return {highOrder:msint_32,
+				lowOrder:lsint_32}
 	},
 	load(){ //инициализация модуля ошибок
 		$.bind_all(sha512);
@@ -296,7 +297,7 @@ let sha512={//(str)
 		lsw=(x.highOrder&0xFFFF)+(y.highOrder&0xFFFF)+(msw>>>16);
 		msw=(x.highOrder>>>16)+(y.highOrder>>>16)+(lsw>>>16);
 		highOrder=((msw&0xFFFF)<<16)|(lsw&0xFFFF);
-		return new this.int64(highOrder,lowOrder);
+		return this.int64(highOrder,lowOrder);
 	},
 	safe_add_4(a,b,c,d){
 		let lsw,
@@ -309,7 +310,7 @@ let sha512={//(str)
 		lsw=(a.highOrder&0xFFFF)+(b.highOrder&0xFFFF)+(c.highOrder&0xFFFF)+(d.highOrder&0xFFFF)+(msw>>>16);
 		msw=(a.highOrder>>>16)+(b.highOrder>>>16)+(c.highOrder>>>16)+(d.highOrder>>>16)+(lsw>>>16);
 		highOrder=((msw&0xFFFF)<<16)|(lsw&0xFFFF);
-		return new this.int64(highOrder, lowOrder);
+		return this.int64(highOrder, lowOrder);
 	},
 	safe_add_5(a,b,c,d,e){
 		let lsw,
@@ -322,25 +323,25 @@ let sha512={//(str)
 		lsw=(a.highOrder&0xFFFF)+(b.highOrder&0xFFFF)+(c.highOrder&0xFFFF)+(d.highOrder&0xFFFF)+(e.highOrder&0xFFFF)+(msw>>>16);
 		msw=(a.highOrder>>>16)+(b.highOrder>>>16)+(c.highOrder>>>16)+(d.highOrder>>>16)+(e.highOrder>>>16)+(lsw>>>16);
 		highOrder=((msw&0xFFFF)<<16)|(lsw&0xFFFF);
-		return new this.int64(highOrder, lowOrder);
+		return this.int64(highOrder, lowOrder);
 	},
 	maj(x,y,z){
-		return new this.int64(
+		return this.int64(
 			(x.highOrder&y.highOrder)^(x.highOrder&z.highOrder)^(y.highOrder&z.highOrder),
 			(x.lowOrder&y.lowOrder)^(x.lowOrder&z.lowOrder)^(y.lowOrder&z.lowOrder));
 	},
 	ch(x,y,z){
-		return new this.int64(
+		return this.int64(
 			(x.highOrder&y.highOrder)^(~x.highOrder&z.highOrder),
 			(x.lowOrder&y.lowOrder)^(~x.lowOrder&z.lowOrder));
 	},
 	rotr(x,n){
 		if(n<=32){
-			return new this.int64(
+			return this.int64(
 				(x.highOrder>>>n)|(x.lowOrder<<(32-n)),
 				(x.lowOrder>>>n)|(x.highOrder<<(32-n)));
 		} else{
-			return new this.int64(
+			return this.int64(
 				(x.lowOrder>>>n)|(x.highOrder<<(32-n)),
 				(x.highOrder>>>n)|(x.lowOrder<<(32-n)));
 		}
@@ -349,7 +350,7 @@ let sha512={//(str)
 		const rotr28=this.rotr(x,28),
 			rotr34=this.rotr(x,34),
 			rotr39=this.rotr(x,39);
-		return new this.int64(
+		return this.int64(
 			rotr28.highOrder^rotr34.highOrder^rotr39.highOrder,
 			rotr28.lowOrder^rotr34.lowOrder^rotr39.lowOrder);
 	},
@@ -357,7 +358,7 @@ let sha512={//(str)
 		const rotr14=this.rotr(x,14),
 			rotr18=this.rotr(x,18),
 			rotr41=this.rotr(x,41);
-		return new this.int64(
+		return this.int64(
 			rotr14.highOrder^rotr18.highOrder^rotr41.highOrder,
 			rotr14.lowOrder^rotr18.lowOrder^rotr41.lowOrder);
 	},
@@ -365,7 +366,7 @@ let sha512={//(str)
 		const rotr1=this.rotr(x,1),
 			rotr8=this.rotr(x,8),
 			shr7=this.shr(x,7);
-		return new this.int64(
+		return this.int64(
 			rotr1.highOrder^rotr8.highOrder^shr7.highOrder,
 			rotr1.lowOrder^rotr8.lowOrder^shr7.lowOrder);
 	},
@@ -373,17 +374,17 @@ let sha512={//(str)
 		const rotr19=this.rotr(x,19),
 			rotr61=this.rotr(x,61),
 			shr6=this.shr(x,6);
-		return new this.int64(
+		return this.int64(
 			rotr19.highOrder^rotr61.highOrder^shr6.highOrder,
 			rotr19.lowOrder^rotr61.lowOrder^shr6.lowOrder);
 	},
 	shr(x,n){
 		if(n<=32){
-			return new this.int64(
+			return this.int64(
 				x.highOrder>>>n,
 				x.lowOrder>>>n|(x.highOrder<<(32-n)));
 		} else{
-			return new this.int64(
+			return this.int64(
 				0,
 				x.highOrder<<(32-n));
 		}
@@ -391,50 +392,50 @@ let sha512={//(str)
 	go(str){
 	const int64=this.int64,
 		safe_add_2=this.safe_add_2;//.bind(this);
-	let H=[new int64(0x6a09e667, 0xf3bcc908), new int64(0xbb67ae85, 0x84caa73b),
-		new int64(0x3c6ef372, 0xfe94f82b), new int64(0xa54ff53a, 0x5f1d36f1),
-		new int64(0x510e527f, 0xade682d1), new int64(0x9b05688c, 0x2b3e6c1f),
-		new int64(0x1f83d9ab, 0xfb41bd6b), new int64(0x5be0cd19, 0x137e2179)],
-	K=[new int64(0x428a2f98, 0xd728ae22), new int64(0x71374491, 0x23ef65cd),
-		new int64(0xb5c0fbcf, 0xec4d3b2f), new int64(0xe9b5dba5, 0x8189dbbc),
-		new int64(0x3956c25b, 0xf348b538), new int64(0x59f111f1, 0xb605d019),
-		new int64(0x923f82a4, 0xaf194f9b), new int64(0xab1c5ed5, 0xda6d8118),
-		new int64(0xd807aa98, 0xa3030242), new int64(0x12835b01, 0x45706fbe),
-		new int64(0x243185be, 0x4ee4b28c), new int64(0x550c7dc3, 0xd5ffb4e2),
-		new int64(0x72be5d74, 0xf27b896f), new int64(0x80deb1fe, 0x3b1696b1),
-		new int64(0x9bdc06a7, 0x25c71235), new int64(0xc19bf174, 0xcf692694),
-		new int64(0xe49b69c1, 0x9ef14ad2), new int64(0xefbe4786, 0x384f25e3),
-		new int64(0x0fc19dc6, 0x8b8cd5b5), new int64(0x240ca1cc, 0x77ac9c65),
-		new int64(0x2de92c6f, 0x592b0275), new int64(0x4a7484aa, 0x6ea6e483),
-		new int64(0x5cb0a9dc, 0xbd41fbd4), new int64(0x76f988da, 0x831153b5),
-		new int64(0x983e5152, 0xee66dfab), new int64(0xa831c66d, 0x2db43210),
-		new int64(0xb00327c8, 0x98fb213f), new int64(0xbf597fc7, 0xbeef0ee4),
-		new int64(0xc6e00bf3, 0x3da88fc2), new int64(0xd5a79147, 0x930aa725),
-		new int64(0x06ca6351, 0xe003826f), new int64(0x14292967, 0x0a0e6e70),
-		new int64(0x27b70a85, 0x46d22ffc), new int64(0x2e1b2138, 0x5c26c926),
-		new int64(0x4d2c6dfc, 0x5ac42aed), new int64(0x53380d13, 0x9d95b3df),
-		new int64(0x650a7354, 0x8baf63de), new int64(0x766a0abb, 0x3c77b2a8),
-		new int64(0x81c2c92e, 0x47edaee6), new int64(0x92722c85, 0x1482353b),
-		new int64(0xa2bfe8a1, 0x4cf10364), new int64(0xa81a664b, 0xbc423001),
-		new int64(0xc24b8b70, 0xd0f89791), new int64(0xc76c51a3, 0x0654be30),
-		new int64(0xd192e819, 0xd6ef5218), new int64(0xd6990624, 0x5565a910),
-		new int64(0xf40e3585, 0x5771202a), new int64(0x106aa070, 0x32bbd1b8),
-		new int64(0x19a4c116, 0xb8d2d0c8), new int64(0x1e376c08, 0x5141ab53),
-		new int64(0x2748774c, 0xdf8eeb99), new int64(0x34b0bcb5, 0xe19b48a8),
-		new int64(0x391c0cb3, 0xc5c95a63), new int64(0x4ed8aa4a, 0xe3418acb),
-		new int64(0x5b9cca4f, 0x7763e373), new int64(0x682e6ff3, 0xd6b2b8a3),
-		new int64(0x748f82ee, 0x5defb2fc), new int64(0x78a5636f, 0x43172f60),
-		new int64(0x84c87814, 0xa1f0ab72), new int64(0x8cc70208, 0x1a6439ec),
-		new int64(0x90befffa, 0x23631e28), new int64(0xa4506ceb, 0xde82bde9),
-		new int64(0xbef9a3f7, 0xb2c67915), new int64(0xc67178f2, 0xe372532b),
-		new int64(0xca273ece, 0xea26619c), new int64(0xd186b8c7, 0x21c0c207),
-		new int64(0xeada7dd6, 0xcde0eb1e), new int64(0xf57d4f7f, 0xee6ed178),
-		new int64(0x06f067aa, 0x72176fba), new int64(0x0a637dc5, 0xa2c898a6),
-		new int64(0x113f9804, 0xbef90dae), new int64(0x1b710b35, 0x131c471b),
-		new int64(0x28db77f5, 0x23047d84), new int64(0x32caab7b, 0x40c72493),
-		new int64(0x3c9ebe0a, 0x15c9bebc), new int64(0x431d67c4, 0x9c100d4c),
-		new int64(0x4cc5d4be, 0xcb3e42b6), new int64(0x597f299c, 0xfc657e2a),
-		new int64(0x5fcb6fab, 0x3ad6faec), new int64(0x6c44198c, 0x4a475817)],
+	let H=[int64(0x6a09e667, 0xf3bcc908), int64(0xbb67ae85, 0x84caa73b),
+		int64(0x3c6ef372, 0xfe94f82b), int64(0xa54ff53a, 0x5f1d36f1),
+		int64(0x510e527f, 0xade682d1), int64(0x9b05688c, 0x2b3e6c1f),
+		int64(0x1f83d9ab, 0xfb41bd6b), int64(0x5be0cd19, 0x137e2179)],
+	K=[int64(0x428a2f98, 0xd728ae22), int64(0x71374491, 0x23ef65cd),
+		int64(0xb5c0fbcf, 0xec4d3b2f), int64(0xe9b5dba5, 0x8189dbbc),
+		int64(0x3956c25b, 0xf348b538), int64(0x59f111f1, 0xb605d019),
+		int64(0x923f82a4, 0xaf194f9b), int64(0xab1c5ed5, 0xda6d8118),
+		int64(0xd807aa98, 0xa3030242), int64(0x12835b01, 0x45706fbe),
+		int64(0x243185be, 0x4ee4b28c), int64(0x550c7dc3, 0xd5ffb4e2),
+		int64(0x72be5d74, 0xf27b896f), int64(0x80deb1fe, 0x3b1696b1),
+		int64(0x9bdc06a7, 0x25c71235), int64(0xc19bf174, 0xcf692694),
+		int64(0xe49b69c1, 0x9ef14ad2), int64(0xefbe4786, 0x384f25e3),
+		int64(0x0fc19dc6, 0x8b8cd5b5), int64(0x240ca1cc, 0x77ac9c65),
+		int64(0x2de92c6f, 0x592b0275), int64(0x4a7484aa, 0x6ea6e483),
+		int64(0x5cb0a9dc, 0xbd41fbd4), int64(0x76f988da, 0x831153b5),
+		int64(0x983e5152, 0xee66dfab), int64(0xa831c66d, 0x2db43210),
+		int64(0xb00327c8, 0x98fb213f), int64(0xbf597fc7, 0xbeef0ee4),
+		int64(0xc6e00bf3, 0x3da88fc2), int64(0xd5a79147, 0x930aa725),
+		int64(0x06ca6351, 0xe003826f), int64(0x14292967, 0x0a0e6e70),
+		int64(0x27b70a85, 0x46d22ffc), int64(0x2e1b2138, 0x5c26c926),
+		int64(0x4d2c6dfc, 0x5ac42aed), int64(0x53380d13, 0x9d95b3df),
+		int64(0x650a7354, 0x8baf63de), int64(0x766a0abb, 0x3c77b2a8),
+		int64(0x81c2c92e, 0x47edaee6), int64(0x92722c85, 0x1482353b),
+		int64(0xa2bfe8a1, 0x4cf10364), int64(0xa81a664b, 0xbc423001),
+		int64(0xc24b8b70, 0xd0f89791), int64(0xc76c51a3, 0x0654be30),
+		int64(0xd192e819, 0xd6ef5218), int64(0xd6990624, 0x5565a910),
+		int64(0xf40e3585, 0x5771202a), int64(0x106aa070, 0x32bbd1b8),
+		int64(0x19a4c116, 0xb8d2d0c8), int64(0x1e376c08, 0x5141ab53),
+		int64(0x2748774c, 0xdf8eeb99), int64(0x34b0bcb5, 0xe19b48a8),
+		int64(0x391c0cb3, 0xc5c95a63), int64(0x4ed8aa4a, 0xe3418acb),
+		int64(0x5b9cca4f, 0x7763e373), int64(0x682e6ff3, 0xd6b2b8a3),
+		int64(0x748f82ee, 0x5defb2fc), int64(0x78a5636f, 0x43172f60),
+		int64(0x84c87814, 0xa1f0ab72), int64(0x8cc70208, 0x1a6439ec),
+		int64(0x90befffa, 0x23631e28), int64(0xa4506ceb, 0xde82bde9),
+		int64(0xbef9a3f7, 0xb2c67915), int64(0xc67178f2, 0xe372532b),
+		int64(0xca273ece, 0xea26619c), int64(0xd186b8c7, 0x21c0c207),
+		int64(0xeada7dd6, 0xcde0eb1e), int64(0xf57d4f7f, 0xee6ed178),
+		int64(0x06f067aa, 0x72176fba), int64(0x0a637dc5, 0xa2c898a6),
+		int64(0x113f9804, 0xbef90dae), int64(0x1b710b35, 0x131c471b),
+		int64(0x28db77f5, 0x23047d84), int64(0x32caab7b, 0x40c72493),
+		int64(0x3c9ebe0a, 0x15c9bebc), int64(0x431d67c4, 0x9c100d4c),
+		int64(0x4cc5d4be, 0xcb3e42b6), int64(0x597f299c, 0xfc657e2a),
+		int64(0x5fcb6fab, 0x3ad6faec), int64(0x6c44198c, 0x4a475817)],
 	W=new Array(64),
 	a,
 	b,
@@ -462,7 +463,7 @@ let sha512={//(str)
 		h=H[7];
 		for(let j=0;j<80;j++){
 			if (j<16){
-				W[j]=new int64(str[j*2+i],str[j*2+i+1]);
+				W[j]=int64(str[j*2+i],str[j*2+i+1]);
 			} else{
 				W[j]=this.safe_add_4(this.gamma1(W[j-2]),W[j-7],this.gamma0(W[j-15]),W[j-16]);
 			}
